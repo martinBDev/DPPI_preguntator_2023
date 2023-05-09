75690788bfcb78f342cb6ff094c1a4a3af8fcf3d
Martin Beltran <UO276244@uniovi.es>
2023-05-09 18:22:25 +0200

    Calculo de puntos

--- script.js
+++ script.js
@@ -20,6 +20,37 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+function calculateScore() {
+    const selectedTopic = questionsData.find(q => q.topic === document.getElementById("topic-selector").value);
+    const totalQuestions = selectedTopic.questions.length;
+    const pointsPerQuestion = 10 / totalQuestions;
+    let score = 0;
+
+    selectedTopic.questions.forEach((question, index) => {
+        const selectedOption = document.querySelector(`input[name="question-${index}"]:checked`);
+
+        if (selectedOption) {
+            const selectedIndex = parseInt(selectedOption.value);
+
+            if (selectedIndex === question.answer) {
+                score += pointsPerQuestion;
+            } else {
+                score -= pointsPerQuestion / totalQuestions;
+            }
+        }
+    });
+
+    // Asegurarse de que la nota esté en el rango de 0 a 10
+    score = Math.min(Math.max(score, 0), 10);
+    alert(`Tu nota es: ${score.toFixed(2)}`);
+}
+
+// Escuchar evento de click en el botón de enviar
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", () => {
+    calculateScore();
+});
+
 let questionsData = [];
 
 function populateTopics(questions) {
